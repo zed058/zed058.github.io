5b9b8f4b35dcc19de3a78441fde6c9b5d6d8870c
稀客 <877058128@qq.com>
2020-03-09 18:42:06 +0800

    web测试

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,15 +1,16 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets\AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "/assets\AssetManifest.json": "4c90a67f8169bac3a7fa280dcb3a60c5",
 "/assets\FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\LICENSE": "1a80be6c5724a31e6f9c9e06dba53b63",
+"/assets\images\logo.png": "61662aaa14b1e82d622e7103a22349b3",
+"/assets\LICENSE": "d74bb323a25cc8df326f745f64c8f452",
 "/assets\packages\cupertino_icons\assets\CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "b535539dcfa16973285efffb7f4740e0",
-"/main.dart.js": "ba2e1c840776d64b82b10e3f03a67c07",
+"/main.dart.js": "3503ea45705c3fb084b6d08584dfce9d",
 "/manifest.json": "9c727ffea20b143a0d0d54013e8dcd68"
 };
 
